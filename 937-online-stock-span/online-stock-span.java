b6e1304ed5c45dd641d8286baf32b245986fb2dd
lathikams513 <lathikams581@gmail.com>
2026-03-13 10:24:55 +0530

    Time: 30 ms (72.68%) | Memory: 54.5 MB (41.98%) - LeetSync

--- 937-online-stock-span/online-stock-span.java
+++ 937-online-stock-span/online-stock-span.java
@@ -13,8 +13,7 @@ class StockSpanner {
         int span = 1;
 
         while(!stack.isEmpty() && stack.peek()[0] <= price){
-            span += stack.peek()[1];
-            stack.pop();
+            span += stack.pop()[1];
         }
 
         stack.push(new int[]{price, span});
