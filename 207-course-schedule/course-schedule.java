3b2c9257920bff9054d6be3749c4436a57ed5264
lathikams513 <lathikams581@gmail.com>
2026-03-25 11:32:15 +0530

    Time: 8 ms (28.41%) | Memory: 47.6 MB (7.50%) - LeetSync

---
+++ 207-course-schedule/course-schedule.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        ArrayList<ArrayList<Integer>> list = new ArrayList<>();
+        for(int i = 0; i < numCourses; i++){
+            list.add(new ArrayList<>());
+        }
+
+        for(int[] n : prerequisites){
+            list.get(n[0]).add(n[1]);
+        }
+        int[] ind = new int[numCourses];
+        for(int i = 0; i < numCourses; i++){
+            for(int nei : list.get(i)){
+                ind[nei]++;
+            }
+        }
+
+        Queue<Integer> q = new LinkedList<>();
+        for(int i = 0; i < numCourses; i++){
+            if(ind[i] == 0){
+                q.add(i);
+            }
+        }
+        int count = 0;
+        while(!q.isEmpty()){
+            int node = q.poll();
+            count++;
+
+            for(int nei : list.get(node)){
+                ind[nei]--;
+                if(ind[nei] == 0){
+                    q.add(nei);
+                }
+            }
+        }
+        return count == numCourses;
+    }
+}
