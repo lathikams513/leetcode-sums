ffd114eca35bfb5af56508bd8979ccc120f91c44
lathikams513 <lathikams581@gmail.com>
2026-03-28 20:30:09 +0530

    Time: 0 ms (100.00%) | Memory: 43.6 MB (35.10%) - LeetSync

---
+++ 27-remove-element/remove-element.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    public int removeElement(int[] nums, int val) {
+
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int num : nums) {
+            if (num != val) {
+                list.add(num);
+            }
+        }
+        for (int i = 0; i < list.size(); i++) {
+            nums[i] = list.get(i);
+        }
+
+        return list.size();
+    }
+}
